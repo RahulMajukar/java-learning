0053807610dfc9f2729a4883b40171c7da31db09
RahulMajukar <rahulmajukar38@gmail.com>
2024-09-21 09:45:43 +0530

    for loop

---
+++ loops/for/OddNum.java
@@ -0,0 +1,11 @@
+public class OddNum {
+
+    public static void main(String[] args) {
+        int num = 0;
+        for (; num < 10; num++) {
+            if (num % 2 != 0)
+                System.out.println(num);
+        }
+    }
+
+}
